e25ab958ce9b09c2a200f33964bf6987de7c00a7
devwizard216 <reubenaslikian@yandex.com>
2023-11-10 02:31:33 +0800

    add const values

--- src/utils/Consts.js
+++ src/utils/Consts.js
@@ -3,4 +3,7 @@ export const STAFF_TYPE_DOCTOR = 'doctor';
 export const STAFF_TYPE_HYGIENE = 'hygiene';
 export const EMPLOYEE_STATUS_PART_TIME = 'part-time';
 export const EMPLOYEE_STATUS_FULL_TIME = 'full-time';
+export const METRICS_PRODUCTION = 'Production';
+export const METRICS_ADJUSTMENTS = 'Adjustments';
+export const METRICS_COLLECTIONS = 'Collections';
 export const SERVER_ADDRESS = 'https://gf57f0wcr1.execute-api.us-east-1.amazonaws.com';
